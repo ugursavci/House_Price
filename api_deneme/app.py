831df561b281c5530623718f2dbbeb99aa7bc520
Ugur Savci <77856368+ugursavci@users.noreply.github.com>
2021-05-12 01:58:38 +0300

    Add files via upload

---
+++ api_deneme/app.py
@@ -0,0 +1,37 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    features = [int(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0])
+
+    return render_template('index.html', prediction='House Price is : {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True,port='2000')
+
+
+
+
+
+
+
+
+
+
